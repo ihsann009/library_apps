2b380fb6dbbdef3df8ca823d7bc0f735c076cdcb
Zpice <ihsanxcaddy009@gmail.com>
2025-05-13 09:18:51 +0800

    update: add entrypoint

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ import Home from './Routes/Home';  // Import Home component from routes folder
 import Library from './Routes/Library';  // Import Library component from routes folder
 import LoginPage from './pages/Login';
 import SidebarNavbar from './Components/Sidebar';
+import ScrollToTop from './Components/ScrollToTop';
 const App = () => {
   return (
     <Router>
+     
       <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50 font-sans">
         {/* Navbar */}
         {/* <Navbar /> */}
