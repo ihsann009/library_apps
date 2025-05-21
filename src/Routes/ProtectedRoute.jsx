02413183dc9b6e829c12f4a128453969f8dc3cc9
unknown <ihsanxcaddy009@gmail.com>
2025-05-21 21:16:27 +0800

    add: protected route for each role

--- src/Routes/ProtectedRoute.jsx
+++ src/Routes/ProtectedRoute.jsx
@@ -1,1 +1,15 @@
- 
+import { useAuth } from '../context/AuthContext';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children, allowedRoles }) => {
+  const { user } = useAuth();
+  if (!user || !allowedRoles.includes(user.role)) {
+    // Redirect ke dashboard sesuai role
+    if (user?.role === 'staff') return <Navigate to="/staff/dashboard" />;
+    if (user?.role === 'admin') return <Navigate to="/admin/dashboard" />;
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
+
+export default ProtectedRoute; 
