828b068b2dfd8c581c08da09b370cdb327d468ca
unknown <ihsanxcaddy009@gmail.com>
2025-05-21 21:24:26 +0800

    add: profile admin

---
+++ src/pages/admin/ProfileAdmin.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const ProfileAdmin = () => (
+  <div className="p-8">
+    <h2 className="text-2xl font-bold mb-4">Profil Admin</h2>
+    <p>Ini halaman profil admin.</p>
+  </div>
+);
+export default ProfileAdmin; 
