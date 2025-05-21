7da19a730d8f3c9294db113c8fe50d436e0c2188
unknown <ihsanxcaddy009@gmail.com>
2025-05-21 21:24:59 +0800

    add: add profile staff

---
+++ src/pages/staff/ProfileStaff.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const ProfileStaff = () => (
+  <div className="p-8">
+    <h2 className="text-2xl font-bold mb-4">Profil Staff</h2>
+    <p>Ini halaman profil staff.</p>
+  </div>
+);
+export default ProfileStaff; 
